f88d92083d715a042e5eb4d7d460f96ca3985ba9
Haerin <soundzrin@naver.com>
2024-03-07 01:22:10 +0900

    2024.03.07

--- Baekjoon/baekjoon25314.java
+++ Baekjoon/baekjoon25314.java
@@ -1,3 +1,5 @@
+package Baekjoon;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -6,9 +8,9 @@ public class baekjoon25314 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n=Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
-        for(int i=4; i<=n ; i+=4){
+        for (int i = 4; i <= n; i += 4) {
             System.out.print("long ");
 
         }
